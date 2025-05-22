0dbde145fe1f8c4299c139ec258c1c525105ee71
Suraj S <surajsureshkumar.98@gmail.com>
2025-05-22 17:06:54 -0400

    Dependency File

---
+++ fetch/src/test/java/takehome/assessment/fetch/FetchApplicationTests.java
@@ -0,0 +1,21 @@
+package takehome.assessment.fetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import takehome.assessment.fetch.entity.ItemEntity;
+import takehome.assessment.fetch.entity.ReceiptEntity;
+import takehome.assessment.fetch.mapper.ReceiptMapper;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class FetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
