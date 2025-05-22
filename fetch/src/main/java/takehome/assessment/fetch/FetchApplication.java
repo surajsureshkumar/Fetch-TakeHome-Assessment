c25b4525b536ac6231835f8d59739177f76d6db0
Suraj S <surajsureshkumar.98@gmail.com>
2025-05-22 16:56:46 -0400

    Spring Boot Application Entry Point

---
+++ fetch/src/main/java/takehome/assessment/fetch/FetchApplication.java
@@ -0,0 +1,13 @@
+package takehome.assessment.fetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchApplication.class, args);
+	}
+
+}
